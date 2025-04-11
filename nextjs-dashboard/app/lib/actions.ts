b8675782d4c54dde47a10668eb8214b779dc4176
Omnia Fathy <omni.fathy@gmail.com>
2025-04-12 01:08:17 +0200

    add auth

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from "zod";
 import { revalidatePath } from "next/cache";
 import postgres from "postgres";
 import { redirect } from "next/navigation";
+import { AuthError } from "next-auth";
+import { signIn } from "@/auth";
 
 const sql = postgres(process.env.DATABASE_URL!, { ssl: "require" });
 
@@ -89,4 +91,20 @@ export async function deleteInvoice(id: string) {
   await sql`Delete From invoices where id = ${id}`;
 
   revalidatePath("/dashboard/invoices");
+}
+
+export async function authenticate(prevState: string | undefined, formData: FormData) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+              case 'CredentialsSignin':
+                return 'Invalid credentials.';
+              default:
+                return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
 }
