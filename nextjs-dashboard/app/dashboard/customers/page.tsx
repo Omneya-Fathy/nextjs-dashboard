4c93230d44f5dab13084e2717b039e71afcb4190
Omnia Fathy <omni.fathy@gmail.com>
2025-04-12 01:22:36 +0200

    add customer page

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,4 +1,24 @@
-export default function CustomersPage() {
-  return <p>Customers Page</p>;
-}
+import { Metadata } from "next";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+import { FormattedCustomersTable } from "@/app/lib/definitions";
+export const metadata: Metadata = {
+  title: "Customers",
+};
 
+export default async function CustomersPage(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+
+  const customers : FormattedCustomersTable[] = await fetchFilteredCustomers(query);
+  return (
+    <div>
+      <CustomersTable customers={customers} />
+    </div>
+  );
+}
